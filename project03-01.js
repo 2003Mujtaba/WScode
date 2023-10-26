13e7cec23f0b41fb887a33c48b54242b0cee07ee
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-10-26 14:13:29 -0400

    hi

---
+++ project03-01.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+      Chapter 3
+      Project 03-01
+
+      Application to calculate total order cost
+      Author: 
+      Date:   
+
+      Filename: project03-01.js
+*/
+
