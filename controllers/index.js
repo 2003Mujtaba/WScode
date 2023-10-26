cf49cbc7103e851a62abeae16b5ee55859b6cc24
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-10-26 19:55:36 -0400

    hi

--- controllers/index.js
+++ controllers/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/home', function(req, res, next) {
-  res.render('layout', { title: 'Home', content: 'home' });
+  res.render('layout', { title: 'Home', content: 'home-content' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('layout', { title: 'About Me', content: 'about' });
+  res.render('layout', { title: 'About Me', content: 'about-section' });
 });
 
 router.get('/projects', function(req, res, next) {
-  res.render('layout', { title: 'Projects', content: 'projects' });
+  res.render('layout', { title: 'Projects', content: 'projects-section' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('layout', { title: 'Contact Me', content: 'contact' });
+  res.render('layout', { title: 'Contact Me', content: 'contact-container' });
 });
 
 router.get('/', function(req, res, next) {
