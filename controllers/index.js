26aa83a5e160a71a99a6a3b59de9d643f093bf19
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-10-26 21:19:02 -0400

    j

--- controllers/index.js
+++ controllers/index.js
@@ -2,27 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/home', function(req, res, next) {
-  console.log("Rendering home with title:", 'Home');
   res.render('layout', { title: 'Home', content: 'home-content' });
 });
 
 router.get('/about', function(req, res, next) {
-  console.log("Rendering home with title:", 'Home');
   res.render('layout', { title: 'About Me', content: 'about-section' });
 });
 
 router.get('/projects', function(req, res, next) {
-  console.log("Rendering home with title:", 'Home');
   res.render('layout', { title: 'Projects', content: 'projects-section' });
 });
 
 router.get('/contact', function(req, res, next) {
-  console.log("Rendering home with title:", 'Home');
   res.render('layout', { title: 'Contact Me', content: 'contact-container' });
 });
 
 router.get('/', function(req, res, next) {
-  console.log("Rendering home with title:", 'Home');
   res.redirect('/home');
 });
 
