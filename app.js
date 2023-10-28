d97e68177c8ccbdf3e959ccde70533f63a8ce4de
2003Mujtaba <144089823+2003Mujtaba@users.noreply.github.com>
2023-10-27 22:00:13 -0400

    ss

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressLayouts = require('express-ejs-layouts');  // <-- Add this line
+var expressLayouts = require('express-ejs-layouts');  
 
 var app = express();
 
-// Static files middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Use express-ejs-layouts
-app.use(expressLayouts);  // <-- Place this before setting views and view engine
+app.use(expressLayouts);  
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const indexRouter = require('./controllers/index');  // Adjust the path if necessary
+const indexRouter = require('./controllers/index');  
 app.use('/', indexRouter);
 
 app.use(logger('dev'));
@@ -26,20 +23,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-// If you have other routers, require and use them here
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
